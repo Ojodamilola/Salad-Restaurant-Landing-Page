2a044a7903e15c5e2244f7a1f094f271b82978c8
Ojo damilola <dammy3114@gmail.com>
2023-12-25 01:01:37 +0100

    I add text typing & reveal on scroll effect

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,4 +10,76 @@ window.addEventListener('scroll', () =>{
    }
 });
 
+// The typewriter element
+var typeWriterElement = document.getElementById('typewriter');
 
+// The TextArray: 
+var textArray = ["__Salad","__Restaurant"];
+
+// You can also do this by transfering it through a data-attribute
+// var textArray = typeWriterElement.getAttribute('data-array');
+
+
+// function to generate the backspace effect 
+function delWriter(text, i, cb) {
+	if (i >= 0 ) {
+		typeWriterElement.innerHTML = text.substring(0, i--);
+		// generate a random Number to emulate backspace hitting.
+ 		var rndBack = 10 + Math.random() * 100;
+		setTimeout(function() {
+			delWriter(text, i, cb);
+		},rndBack); 
+	} else if (typeof cb == 'function') {
+		setTimeout(cb,1000);
+	}
+};
+
+// function to generate the keyhitting effect
+function typeWriter(text, i, cb) {
+	if ( i < text.length+1 ) {
+		typeWriterElement.innerHTML = text.substring(0, i++);
+		// generate a random Number to emulate Typing on the Keyboard.
+		var rndTyping = 250 - Math.random() * 100;
+		setTimeout( function () { 
+			typeWriter(text, i++, cb)
+		},rndTyping);
+	} else if (i === text.length+1) {
+		setTimeout( function () {
+			delWriter(text, i, cb)
+		},1000);
+	}
+};
+
+// the main writer function
+function StartWriter(i) {
+	if (typeof textArray[i] == "undefined") {
+		setTimeout( function () {
+			StartWriter(0)
+		},1000);
+	} else if(i < textArray[i].length+1) {
+		typeWriter(textArray[i], 0, function () {
+			StartWriter(i+1);
+		});
+	}  
+};
+// wait one second then start the typewriter
+setTimeout( function () {
+	StartWriter(0);
+},1000);
+	
+// Reveal on Scroll
+ ScrollReveal({
+	reset: true,
+	distance: '60px',
+	duration: 2500,
+	delay: 400
+ });
+ ScrollReveal().reveal('.rightReveal', {delay:500, origin:'right'});
+ ScrollReveal().reveal('.leftReveal', {delay:500, origin:'left'});
+ ScrollReveal().reveal('.bottomReveal', {delay:500, origin:'bottom'});
+ ScrollReveal().reveal('.topReveal', {delay:500, origin:'top'});
+ ScrollReveal().reveal('.liReveal img', {delay:500, origin:'top', interval:200});
+
+
+ // Dark & light mode toggle
+let toggleButton = document.getElementById('toggleButton');
